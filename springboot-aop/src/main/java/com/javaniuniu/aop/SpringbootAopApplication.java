e965a954034d1b2ac52184d43acddd19a7d8a570
minp <king101125s@gmail.com>
2020-02-22 23:22:53 +0800

    springboot aop演示-日志篇
    springboot aop演示-日志篇

---
+++ springboot-aop/src/main/java/com/javaniuniu/aop/SpringbootAopApplication.java
@@ -0,0 +1,14 @@
+package com.javaniuniu.aop;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAopApplication.class, args);
+    }
+
+}
