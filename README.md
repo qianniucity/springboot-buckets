62b71c964a4da98b5d3645b75b828a32812c32cc
minp <king101125s@gmail.com>
2020-02-27 15:19:01 +0800

    add springboot-aop-log
    add springboot-aop-log

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 | 项目名称 | 文章地址 |
 |:--- |:---   |
-|springboot-aop|[SpringBoot AOP演示项目-日志篇](./springboot-aop)|
+|springboot-aop-|[SpringBoot AOP演示项目-日志篇](./springboot-aop-log)|
 |springboot-async|[异步线程池-三种方式](./springboot-async)|
 |springboot-batch|[SpringBoot批处理（未完成）](./springboot-batch)|
 |springboot-cache|[SpringBoot Cache 演示项目](./springboot-cache)|
---
+++ springboot-aop-log/src/main/java/com/javaniuniu/aop/swagger2/Swagger2Config.java
@@ -0,0 +1,51 @@
+package com.javaniuniu.aop.swagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+	/**
+	 * 通过 createRestApi函数来构建一个DocketBean
+	 * 函数名,可以随意命名,喜欢什么命名就什么命名
+	 */
+	@Bean
+    public Docket createRestApiAop_1() {
+    	return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.javaniuniu.aop.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class)
+				.useDefaultResponseMessages(false)
+				.enableUrlTemplating(true);
+    }
+    //构建 api文档的详细信息函数
+	@SuppressWarnings("deprecation")
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				//页面标题
+				.title("SpringBoot Aop演示")
+				//创建人
+				.contact("javaniuniu")
+				//版本号
+				.version("1.0")
+				//描述
+				.description("Aop初始化api")
+				.build();
+	}
+}
